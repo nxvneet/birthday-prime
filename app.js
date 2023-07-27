2d71ce895c6ec99a4133a160b62ddf65363c24be
Navneet Singh <mufc.navneet@gmail.com>
2023-07-27 20:29:16 +0530

    refactor

--- app.js
+++ app.js
@@ -7,42 +7,56 @@ var outputDiv2 = document.querySelector("#output2");
 // var day;
 // var month;
 
+// function validation(){
 
-function checkPrime(number){
+//     if(userName != "[a-zA-Z]"){
+//         alert("Username can't have anything other than a-zA-Z - Try again!");
+//         return false;
+//     }
+//     else if(uDob != "[0-9]"){
+//         alert("Date of birth can be added in DDMM format only");
+//         return false;
+//     }
+//     else{
+//         return true;
+//     }
 
-    if(number <= 1){
+// }
+
+function checkPrime(number) {
+
+    if (number <= 1) {
         return false;
     }
-        for(var i= 2 ; i*i<= number ; i++){
-            if(number % i === 0){
-                return false;
-            }
+    for (var i = 2; i * i <= number; i++) {
+        if (number % i === 0) {
+            return false;
         }
+    }
     return true;
 
 }
 
-function dateOfBirthPrime(userDob){
+function dateOfBirthPrime(userDob) {
 
-var day = Number(userDob.slice(0, 2));
-var month = Number(userDob.slice(2, 4));
+    var day = Number(userDob.slice(0, 2));
+    var month = Number(userDob.slice(2, 4));
 
-return checkPrime(day) && checkPrime(month);
+    return checkPrime(day) && checkPrime(month);
 
 }
 
 
-function result(){
+function result() {
     var userDob = uDob.value;
-    if(dateOfBirthPrime(userDob) == true){
-        outputDiv.innerHTML = userName.value + " your date of birth is" + "<span class='prime-txt'> PRIME</span>";
-   
+    if (dateOfBirthPrime(userDob) == true) {
+        outputDiv.innerHTML = userName.value + " your date of birth is " + "<span class='prime-txt'> PRIME</span>";
+
+    } else {
+        outputDiv.innerHTML = userName.value + " your date of birth is" + " <span class='not-prime-txt'> NOT PRIME</span>";
+
     }
-   else{
-    outputDiv.innerHTML = userName.value + " your date of birth is" + " <span class='not-prime-txt'> NOT PRIME</span>";
-   
-   } 
-    
+
 }
 
 btnResult.addEventListener("click", result);
